f379b2d0bcef9af12fc307b195281ffe03394ee7
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-09-19 12:10:09 -0600

    feat: :construction: Setup projects and folder structure
    Backend and frontend ready to code

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+const PORT = app.get('port');
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
