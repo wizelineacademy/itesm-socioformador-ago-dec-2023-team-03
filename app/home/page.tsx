3d4cf056c3a041e14a1eea6eb04f1bb9dfa76f27
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-10-02 11:03:45 -0600

    feat: :art: Move components into the home page
    Now the projects is more structure

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,25 +1,22 @@
 "use client";
 
-import Box from "@/app/components/Box";
-import Sidebar from '@/app/components/Sidebar'
-import Header from '@/app/components/Header'
+import Header from '@/app/components/Header';
+import Sidebar from '@/app/components/Sidebar';
+import ChatBox from "@/components/ChatBox";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
     const { user, error, isLoading } = useUser();
 
-    if(error) { console.log(error) }
-    if(isLoading) { return <div className="loader"></div> }
-    
+    if (error) { console.log(error) }
+    if (isLoading) { return <div className="loader"></div> }
+
     return (
         <Sidebar>
             <Header>
                 <div className="h-full w-full bg-black">
-                    <Box className="h-full w-full bg-black">
-                        Chat Box
-                    </Box>
+                    <ChatBox />
                 </div>
             </Header>
         </Sidebar>
-    )
-  }
+}
