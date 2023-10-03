8c71c854438bad112e16a1b960adc8e0d81fc759
a01276097 <72992464+a01276097@users.noreply.github.com>
2023-10-03 11:22:04 -0600

    Update page.tsx
    fix unauthorized access

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,14 +4,23 @@ import ChatBox from "@/app/components/ChatBox";
 import Header from '@/app/components/Header';
 import Sidebar from '@/app/components/Sidebar';
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { redirect, usePathname} from "next/navigation";
 import Box from "../components/Box";
 
 export default function Home() {
     const { user, error, isLoading } = useUser();
+    const pathname = usePathname();
 
     if (error) { console.log(error) }
     if (isLoading) { return <div className="loader"></div> }
 
+    if(!user) {
+        return (
+            <>
+                {redirect("/login")}
+            </>
+        )
+    } else {
     return (
             <Sidebar>
                 <Header>
@@ -19,4 +28,5 @@ export default function Home() {
                 </Header>
             </Sidebar>
     )
+    }
 }
