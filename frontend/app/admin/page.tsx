987c0df485666a98c9ae42cca2e01393d321c895
EmilioRivmon <fhjzk_dvzo@hotmail.com>
2023-11-02 10:46:03 -0600

    dashboard teams

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -21,13 +21,39 @@ export default function Home() {
             </>
         )
     } else {
+
+        const existingGroups = [
+            { id: 1, name: "Group 1" },
+            { id: 2, name: "Group 2" },
+            { id: 3, name: "Equipo alfa buena onda maravilla hombre lobo" },
+          ];
+
+
+
     return (
       <div className="h-full">
         <Sidebar reference={pathname}>
             <Header>
                 <div className="h-full">
-                    <Box className="h-full bg-black">
-                        Admin Dashboard
+                    <Box className="h-full bg-white">
+                        <div className= "flex justify-end items-center p-10"> 
+                        <input type="text" className="px-3 py-2 m-3 bg-gray-400 rounded-xl w-1/3" placeholder="Search..."/>
+                                <button className="create-group-button px-3 py-2 m-3 bg-blue-500 rounded-xl w-1/6" onClick={handleCreateGroup}>
+                              + Create Group
+                            </button>
+                        </div>
+                    <div  className="existing-groups">
+                            {existingGroups.map((group) => (
+                                <div key={group.id} style={{ backgroundColor: 'rgb(34, 37, 41)' }} className="group-item flex items-center px-10 py-2 m-3 rounded-xl ">
+                                   <div className="ml-10 ">
+                                    {group.name}
+                                    </div>
+                                    <button style={{ backgroundColor: 'rgb(233, 61, 68)' }} className="delete-group-button rounded-2xl px-4 py-2  w-20 ml-auto" onClick={() => handleDeleteGroup(group.id)}>
+                                        X
+                                        </button>
+                                </div>
+                             ))} 
+                        </div>                      
                     </Box>
                 </div>
             </Header>
@@ -35,4 +61,8 @@ export default function Home() {
       </div>
     )
     }
+
+
+  }function handleCreateGroup() {
+    // logic for groups 
   }
