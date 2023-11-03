0d1313f484b69a12cc9dc425cdb6bccb481ce2af
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-11-02 18:59:37 -0600

    Refactor componnent

--- frontend/app/components/Filter.tsx
+++ frontend/app/components/Filter.tsx
@@ -1,62 +1,120 @@
+"use client";
+
+import { useState } from "react";
+
+type Option = {
+  id: string;
+  label: string;
+}
+
+const orders: Option[] = [
+  { id: "low-hight", label: "Tokens: Low-high" },
+  { id: "hight-low", label: "Tokens: High-low" },
+  { id: "name-a-z", label: "Name: A-Z" },
+  { id: "name-z-a", label: "Name: Z-A" },
+  { id: "group-a-z", label: "Group: A-Z" },
+  { id: "group-z-a", label: "Group: Z-A" },
+];
+
+const groups: string[] = ['A', 'B', 'C', 'D', 'E'];
+
+const tokens: Option[] = [
+  { id: 'none', label: 'None' },
+  { id: '1-15', label: '1-15' },
+  { id: '16-50', label: '16-50' },
+  { id: 'more-50', label: '50+' },
+];
 
 export default function Filter() {
+  const [orderBy, setOrderBy] = useState(orders[0].id);
+  const [selectedGroups, setSelectedGroups] = useState<string[]>([`group-${groups[0]}`]);
+  const [selectedTokens, setSelectedTokens] = useState<string[]>([`tokens-${tokens[0].id}`]);
+
+  const handleOrderByChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrderBy(e.target.value);
+  }
+
+  const handleGroupsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setSelectedGroups(prev => [...prev, e.target.value]);
+    } else {
+      setSelectedGroups(prev => prev.filter((group) => group !== e.target.value));
+    }
+  }
+
+  const handleTokensChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setSelectedTokens(prev => [...prev, e.target.value]);
+    } else {
+      setSelectedTokens(prev => prev.filter((token) => token !== e.target.value));
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit");
+    console.log("Order By: ", orderBy);
+    console.log("Group: ", groups);
+    console.log("Token: ", tokens);
+  }
+
   return (
     <div>
       <h1>Filter</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>Order by</legend>
-          <input type="radio" id="low-hight" name="order-by" value="low-hight" />
-          <label htmlFor="low-hight">Tokens: Low-high</label>
-          <br />
-          <input type="radio" id="hight-low" name="order-by" value="hight-low" />
-          <label htmlFor="hight-low">Tokens: High-low</label>
-          <br />
-          <input type="radio" id="name-a-z" name="order-by" value="name-a-z" />
-          <label htmlFor="name-a-z">Name: A-Z</label>
-          <br />
-          <input type="radio" id="name-z-a" name="order-by" value="name-z-a" />
-          <label htmlFor="name-z-a">Name: Z-A</label>
-          <br />
-          <input type="radio" id="group-a-z" name="order-by" value="group-a-z" />
-          <label htmlFor="group-a-z">Group: A-Z</label>
-          <br />
-          <input type="radio" id="group-z-a" name="order-by" value="group-z-a" />
-          <label htmlFor="group-z-a">Group: Z-A</label>
+          {
+            orders.map((order) => (
+              <div key={order.id}>
+                <input
+                  type="radio"
+                  id={order.id}
+                  name="order-by"
+                  value={order.id}
+                  checked={orderBy === order.id}
+                  onChange={handleOrderByChange}
+                />
+                <label htmlFor={order.id}>{order.label}</label>
+              </div>
+            ))
+          }
         </fieldset>
         <fieldset>
           <legend>Groups</legend>
-          <input type="checkbox" id="group-a" name="group-a" value="group-a" />
-          <label htmlFor="group-a">Group A</label>
-          <br />
-          <input type="checkbox" id="group-b" name="group-b" value="group-b" />
-          <label htmlFor="group-b">Group B</label>
-          <br />
-          <input type="checkbox" id="group-c" name="group-c" value="group-c" />
-          <label htmlFor="group-c">Group C</label>
-          <br />
-          <input type="checkbox" id="group-d" name="group-d" value="group-d" />
-          <label htmlFor="group-d">Group D</label>
-          <br />
-          <input type="checkbox" id="group-e" name="group-e" value="group-e" />
-          <label htmlFor="group-e">Group E</label>
-          <br />
+          {
+            groups.map((group) => (
+              <div key={group}>
+                <input
+                  type="checkbox"
+                  id={`group-${group}`}
+                  name={`group-${group}`}
+                  value={`group-${group}`}
+                  onChange={handleGroupsChange}
+                />
+                <label htmlFor={`group-${group}`}>Group {group}</label>
+              </div>
+            ))
+          }
         </fieldset>
         <fieldset>
           <legend>Tokens</legend>
-          <input type="checkbox" id="tokens-none" name="tokens-none" value="tokens-none" />
-          <label htmlFor="none">None</label>
-          <br />
-          <input type="checkbox" id="tokens-1-15" name="tokens-1-15" value="tokens-1-15" />
-          <label htmlFor="tokens-1-15">1-15</label>
-          <br />
-          <input type="checkbox" id="tokens-16-50" name="tokens-16-50" value="tokens-16-50" />
-          <label htmlFor="tokens-16-50">16-50</label>
-          <br />
-          <input type="checkbox" id="tokens-more-50" name="tokens-more-50" value="tokens-more-50" />
-          <label htmlFor="tokens-more-50">50+</label>
-          <br />
+          {
+            tokens.map((token) => (
+              <div key={token.id}>
+                <input
+                  type="checkbox"
+                  id={`token-${token.id}`}
+                  name={`token-${token.id}`}
+                  value={`token-${token.id}`}
+                  onChange={handleTokensChange}
+                />
+                <label htmlFor={token.id}>{token.label}</label>
+              </div>
+            ))
+          }
         </fieldset>
+        <button type="submit">Apply</button>
       </form>
     </div>
   );
