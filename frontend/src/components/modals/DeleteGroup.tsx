dcd6b0278e23a163c7e924bc56b370d56d7fe1ce
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-11-10 19:42:39 -0600

    feat: :sparkles: Modals
    Modals are now ready to use

---
+++ frontend/src/components/modals/DeleteGroup.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+interface NewGroupProps {
+  close: () => void;
+}
+
+export default function DeleteGroup({ close }: NewGroupProps) {
+  const handleClose = () => {
+    close();
+  }
+  const handleDelete = async () => {
+    try {
+      const res = await fetch('/api/groups', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const data = await res.json();
+      console.log(data);
+      handleClose();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <>
+      <header>
+        <h1 className="text-center text-lg font-semibold">Warning</h1>
+      </header>
+      <div>
+        <p>Are you sure you want to delete this group?</p>
+      </div>
+      <footer className="flex justify-around">
+        <button onClick={() => handleClose()} className="text-white bg-gray-500 py-1 px-4 rounded-2xl hover:bg-gray-700">Go back</button>
+        <button onClick={() => handleDelete()} className="text-white bg-red-500 py-1 px-4 rounded-2xl hover:bg-red-700">Delete</button>
+      </footer>
+    </>
+  );
+}
