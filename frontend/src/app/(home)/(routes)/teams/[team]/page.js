7beb405ea35c4100a71a9baf078030f822709b2c
Juan_Bedoya_Barbosa <102477075+JBedoya-Barbosa@users.noreply.github.com>
2023-11-21 10:17:11 -0600

    improving user dashboard

--- frontend/src/app/(home)/(routes)/teams/[team]/page.js
+++ frontend/src/app/(home)/(routes)/teams/[team]/page.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import services from '../../../../services';
+import services from '../../../../../services';
+import LlmsList from './llms-list';
 
 export default async function TeamPage(props) {
   const teamId = props.params.team;
@@ -7,8 +7,10 @@ export default async function TeamPage(props) {
   const llms = llmsResponse.data.llms;
 
   return (
-    <div className='flex flex-col h-full'>
-      {
+    <div className='w-full flex flex-col h-full'>
+      <h1 className=' text-4xl font-semibold m-5'>Team models</h1>
+      <LlmsList llms={llms} teamId={teamId} />
+      {/* {
         llms.length > 0
           ? (
             <ul className='flex flex-col gap-y-1 h-full'>
@@ -16,6 +18,7 @@ export default async function TeamPage(props) {
                 <li key={`${idx}-${llm.id}`}>
                   <Link href={`/chat?team-id=${teamId}&llm-id=${llm.id}`} className='font-medium w-full flex flex-col p-5 bg-regal-blue-dark hover:bg-regal-blue-light'>
                     {llm.name}
+                    <span className=' text-xs font-light'>{llm.model}</span>
                     <span className='mt-1 font-normal text-xs text-yellow-500'>50 tokens</span>
                   </Link>
                 </li>
@@ -23,7 +26,7 @@ export default async function TeamPage(props) {
             </ul>
           )
           : <h1>Team does not have access to any LLM</h1>
-      }
+      } */}
     </div>
   );
 }
--- frontend/src/app/(home)/_components/sidebar/index.js
+++ frontend/src/app/(home)/_components/sidebar/index.js
@@ -1,7 +1,11 @@
+'use client';
+
 import Link from 'next/link'
 
 import { HiOutlineUserGroup } from 'react-icons/hi';
 import { AiOutlineUser } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import { BiLogOut } from "react-icons/bi";
 
 const categories = [
   {
@@ -17,14 +21,19 @@ const categories = [
 ]
 
 function Sidebar() {
+  const pathname = usePathname();
+  const selectedCategory = pathname.split('/')[1];
+
   return (
-    <aside className='bg-regal-blue-dark flex flex-col w-64 h-full shrink-0 border-r border-gray-600'>
-      <ul className='overflow-y-scroll h-full px-4'>
+    <aside className='bg-regal-blue-dark flex flex-col w-64 h-full shrink-0 border-r border-gray-600 p-4'>
+      <ul className='overflow-y-scroll h-full'>
         {categories.map((category, idx) => (
           <li key={`${category}-${idx}`}>
             <Link
               href={`/${category.slug}`}
-              className='flex w-full rounded-md px-2 py-3 items-center gap-x-2 hover:bg-regal-blue-light'
+              className={('flex w-full rounded-md px-2 py-3 items-center gap-x-2 hover:bg-regal-blue-light' +
+                `${selectedCategory === category.slug ? ' bg-regal-blue-light' : ' '}`
+              )}
             >
               {<category.Icon size={24} color='#FFFFFF' />}
               {category.name}
@@ -32,7 +41,10 @@ function Sidebar() {
           </li>
         ))}
       </ul>
-      <Link href='/api/auth/logout'>Logout</Link>
+      <Link href='/api/auth/logout' className='text-brand-primary w-full text-lg font-medium flex items-center gap-x-3'>
+        <BiLogOut color={'#E93D44'} size={28} />
+        Logout
+      </Link>
     </aside>
   )
 }
