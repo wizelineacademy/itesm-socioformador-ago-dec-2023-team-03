d7fbf032f447716c0aea40ffef7dfed0d3064bca
Arturo <A01177145@tec.mx>
2023-11-21 18:42:34 -0600

    UI Changes

--- frontend/src/app/components/AdminMembersList.tsx
+++ frontend/src/app/components/AdminMembersList.tsx
@@ -28,7 +28,7 @@ const AdminMemberList: React.FC<AdminMemberListProps> = ({
                 <p>{member?.firstName ?? "First Name"}</p>
                 <p>{member?.lastName ?? "Last Name"}</p>
             </div>
-            <button onClick={() => handleRemoveMember} className="btn btn-circle btn-sm pointer-events-auto">
+            <button onClick={handleRemoveMember} className="btn btn-circle btn-sm pointer-events-auto">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
